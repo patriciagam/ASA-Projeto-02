1368ec1c291fd0a25c882dd87029587db031401d
Patrícia <patricia.a.gameiro@gmail.com>
2023-12-16 19:47:23 +0000

    probably not efficient enough - bad names and organization

--- p2.cpp
+++ p2.cpp
@@ -1,117 +1,134 @@
-#include <algorithm>
 #include <iostream>
-#include <list>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
-class Graph {
-public:
-    unordered_map<int, bool> visited;
-    unordered_map<int, list<int>> adj;
-    unordered_set<int> inCurrentPath;  // To detect cycles
-    unordered_set<int> cycleNodes;     // To mark nodes in a cycle
-
-    void addEdge(int x, int y) { adj[x].push_back(y); }
-    void dfs(int node, stack<int>& s);
-    bool hasCycle();
-
-    // Create a new graph with cycles collapsed
-    Graph createGraphWithCollapsedCycles();
-};
-
-void Graph::dfs(int node, stack<int>& s) {
+void dfs1(int node, const vector<vector<int>>& graph, vector<bool>& visited, stack<int>& st) {
     visited[node] = true;
-    inCurrentPath.insert(node);
 
-    for (int neighbor : adj[node]) {
+    for (int neighbor : graph[node]) {
         if (!visited[neighbor]) {
-            dfs(neighbor, s);
-        } else if (inCurrentPath.count(neighbor)) {
-            cycleNodes.insert(node);  // Mark node as part of the cycle
+            dfs1(neighbor, graph, visited, st);
         }
     }
 
-    inCurrentPath.erase(node);
-    s.push(node);
+    st.push(node);
 }
 
-bool Graph::hasCycle() {
-    for (const auto& entry : adj) {
-        int node = entry.first;
-        if (!visited[node] && !cycleNodes.count(node)) {
-            stack<int> s;
-            dfs(node, s);
+void dfs2(int node, const vector<vector<int>>& reversedGraph, vector<bool>& visited, vector<int>& scc, int sccNumber) {
+    visited[node] = true;
+    scc[node] = sccNumber;
+
+    for (int neighbor : reversedGraph[node]) {
+        if (!visited[neighbor]) {
+            dfs2(neighbor, reversedGraph, visited, scc, sccNumber);
+        }
+    }
+}
+
+vector<vector<int>> compressSCCs(int n, const vector<pair<int, int>>& relationships, const vector<int>& scc) {
+    vector<vector<int>> compressedGraph(n + 1);
+
+    for (const auto& rel : relationships) {
+        int x = rel.first;
+        int y = rel.second;
+        int sccX = scc[x];
+        int sccY = scc[y];
+
+        if (sccX != sccY) {
+            compressedGraph[sccX].push_back(sccY);
         }
     }
 
-    return !cycleNodes.empty();
+    return compressedGraph;
 }
 
-Graph Graph::createGraphWithCollapsedCycles() {
-    Graph newGraph;
+vector<vector<int>> findSCCs(int n, const vector<pair<int, int>>& relationships, vector<int>& scc) {
+    vector<vector<int>> graph(n + 1);
+    vector<vector<int>> reversedGraph(n + 1);
 
-    for (const auto& entry : adj) {
-        int node = entry.first;
-        if (!cycleNodes.count(node)) {
-            for (int neighbor : adj[node]) {
-                if (!cycleNodes.count(neighbor)) {
-                    newGraph.addEdge(node, neighbor);
-                }
-            }
-        }
+    for (const auto& rel : relationships) {
+        int x = rel.first;
+        int y = rel.second;
+        graph[x].push_back(y);
+        reversedGraph[y].push_back(x);
     }
 
-    return newGraph;
-}
-
-int maxJumps(Graph& graph, int n) {
-    vector<int> distances(n + 1, 0);
-    stack<int> s;
+    vector<bool> visited(n + 1, false);
+    stack<int> st;
 
     for (int i = 1; i <= n; ++i) {
-        if (!graph.visited[i]) {
-            graph.dfs(i, s);
+        if (!visited[i]) {
+            dfs1(i, graph, visited, st);
         }
     }
 
-    // Create a new graph with cycles collapsed
-    Graph newGraph = graph.createGraphWithCollapsedCycles();
+    fill(visited.begin(), visited.end(), false);
+    int sccNumber = 0;
 
-    while (!s.empty()) {
-        int current = s.top();
-        s.pop();
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
 
-        for (int neighbor : newGraph.adj[current]) {
-            distances[neighbor] = max(distances[neighbor], distances[current] + 1);
+        if (!visited[node]) {
+            ++sccNumber;
+            dfs2(node, reversedGraph, visited, scc, sccNumber);
         }
     }
 
-    return *max_element(distances.begin(), distances.end());
+    // Return the compressed graph of SCCs
+    return compressSCCs(n, relationships, scc);
 }
 
+
+int dfs(int node, const vector<vector<int>>& graph, vector<bool>& visited) {
+    visited[node] = true;
+
+    int max_jumps = 0;
+
+    for (int neighbor : graph[node]) {
+        if (!visited[neighbor]) {
+            int jumps = 1 + dfs(neighbor, graph, visited);
+            max_jumps = max(max_jumps, jumps);
+        } else {
+            max_jumps = max(max_jumps, 1);
+        }
+    }
+
+    return max_jumps;
+}
+
+int calculateMaxJumps(int n, const vector<pair<int, int>>& relationships) {
+    vector<int> scc(n + 1, 0);
+    vector<vector<int>> sccGraph = findSCCs(n, relationships, scc);
+
+    int sccCount = *max_element(scc.begin(), scc.end());
+
+    int max_jumps = 0;
+
+    for (int start_scc = 1; start_scc <= sccCount; ++start_scc) {
+        vector<bool> visited(sccCount + 1, false);
+        max_jumps = max(max_jumps, dfs(start_scc, sccGraph, visited));
+    }
+
+    return max_jumps;
+}
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    Graph graph;
+    vector<pair<int, int>> relationships(m);
 
     for (int i = 0; i < m; ++i) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        graph.addEdge(x, y);
+        scanf("%d %d", &relationships[i].first, &relationships[i].second);
     }
 
-    // Create a new graph with cycles collapsed
-    Graph collapsedGraph = graph.createGraphWithCollapsedCycles();
+    int result = calculateMaxJumps(n, relationships);
 
-    int result = maxJumps(collapsedGraph, n);
     printf("%d\n", result);
 
     return 0;
 }
-
